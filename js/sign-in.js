830f160f250014c5a1d6285abebb4f6bd757d03a
ogodo kelvin <kelvinchukwuebuka385@gmail.com>
2021-04-26 03:29:34 -0700

    changed css,js and html

---
+++ js/sign-in.js
@@ -0,0 +1,17 @@
+
+
+
+function scrollApear(){
+
+var text=document.getElementById('text');
+
+var textPosition=text.getBoundingClientRect().top;
+
+var screenPosition=window.innerHeight;
+
+    if(textPosition < screenPosition){
+        text.classList.add('animation');
+    }
+}
+
+window.addEventListener('scroll',scrollApear);
